8edd8aecc14877eb108244898e9112b6cc684a13
khal0509 <khal0509@stud.kea.dk>
2022-09-27 14:02:55 +0200

    second

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,2 +1,11 @@
 public class Adventure {
+    Room room1 = new Room("Room 1", "idk");
+    Room room2 = new Room("Room 2", "idk");
+    Room room3 = new Room("Room 3", "idk");
+    Room room4 = new Room("Room 4", "idk");
+    Room room5 = new Room("Room 5", "idk");
+    Room room6 = new Room("Room 6", "idk");
+    Room room7 = new Room("Room 7", "idk");
+    Room room8 = new Room("Room 8", "idk");
+    Room room9 = new Room("Room 9", "idk");
 }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,16 @@
 public class Room {
+    private String name;
+
+    private String description;
+    private Room North;
+    private Room East;
+    private Room West;
+    private Room South;
+
+    public Room(String name, String Description) {
+        this.name = name;
+        this.description = description;
+    }
+
+
 }
